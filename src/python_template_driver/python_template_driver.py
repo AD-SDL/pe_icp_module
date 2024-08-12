d213b93e3978b55cf0d9b91184aa37dfbf0df73b
Ryan D. Lewis <accounts+github@ryandlewis.dev>
2024-08-12 11:58:57 -0400

    Initial commit

---
+++ src/python_template_driver/python_template_driver.py
@@ -0,0 +1,22 @@
+"""Driver for interfacing with the python_template device/instrument/robot."""
+
+from pathlib import Path
+
+from starlette.datastructures import State
+
+# * Using .dlls and .NET assemblies
+# * pip install pythonnet
+# * See docs: https://pythonnet.github.io/pythonnet/python.html
+# import clr
+# clr.AddReference("Your.Assembly")
+# from Your.Driver.Namespace import DriverClass
+
+
+def run_protocol(path: Path):
+    """Run a protocol file"""
+    pass
+
+
+def query_state(state: State):
+    """Update the state by querying the device"""
+    pass
